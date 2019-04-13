381eff4c18e3e7e947404e3804b01f174caa6c83
anaanthan <49579449+anaanthan@users.noreply.github.com>
2019-04-13 19:16:13 +0800

    Add files via upload

---
+++ plot.R
@@ -0,0 +1,21 @@
+## Anaanthan 
+
+# Loading data (diamonds data comes with ggplot2 package)
+library(ggplot2)
+data=diamonds
+
+# Data exploration
+str(diamonds)
+summary(diamonds)
+
+# using base plot
+plot(data$carat, data$price)
+
+# using ggplot
+plot=ggplot(data = data, aes(x= carat, y=price))
+plot + geom_point()
+
+# adding informations on color of diamonds
+plot2= ggplot(data = data, aes(x= carat, y=price, color= color))
+plot2 + geom_point()
+plot2 + geom_point() + ggtitle( "Price of Diamonds")
