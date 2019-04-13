c442f0749dbd223b5857309f837f3eed5ec50d01
anaanthan <49579449+anaanthan@users.noreply.github.com>
2019-04-13 18:32:53 +0800

    Add files via upload

---
+++ Correlations.R
@@ -0,0 +1,21 @@
+## Anaanthan ##
+
+# Loading data 
+# dataset diamond comes with ggplot2 package
+library(ggplot2)
+data= diamonds
+
+# Data exploration
+str(diamonds)
+summary(diamonds)
+
+# Please be noted carat means weight of diamond
+
+# Pearson Correlation (Price vs Weight)
+cor.test(data$carat, data$price)
+
+# Spearman Correlation (Price vs Weight)
+cor.test(data$carat, data$price, method = "spearman")
+
+# by default corr test is pearson even if we dont specify method
+
