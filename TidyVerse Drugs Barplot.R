3664c2b538278c70d9fe2d45584d4b325b417eaf
anaanthan <49579449+anaanthan@users.noreply.github.com>
2019-04-13 22:51:12 +0800

    Add files via upload

---
+++ TidyVerse Drugs Barplot.R
@@ -0,0 +1,17 @@
+## you need to install tidyverse first if it was not installed 
+library(tidyverse)
+#Create dataframe: Hypothetical
+Test <- tribble(
+  ~DrugClass, ~Value, ~Glycemic_control,
+  "A", 20, "Controlled",
+  "B", 14, "Controlled",
+  "C", 23, "Controlled",
+  "A", 15, "Uncontrolled",
+  "B", 9, "Uncontrolled",
+  "C", 18, "Uncontrolled") %>% 
+  mutate(
+    Class = as.factor(Class))
+View(Test)
+ggplot(data = Test, aes(x = Class, y = Value, fill = Glycemic_control))+
+  geom_bar(stat = "identity", position = "dodge")
+
